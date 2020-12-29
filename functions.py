153774e04df1e4a13972f534b85be95518c045d4
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-29 16:06:37 +0900

    code : add comment in functions.py

--- functions.py
+++ functions.py
@@ -20,15 +20,20 @@ def xls_file_to_list(path: str):
     return [load_ws.row_values(i) for i in range(load_ws.nrows)]
 
 
-def list_data_preprocessing(sheet: list):
+def list_data_preprocessing(sheet: list, add_row_header: bool):
     """
     2차원 리스트(:param sheet:)를 받아
     날짜별 식단과 그의 영양소만 추출해 반환(:return:)하는 함수
+    (:param add_row_header:가 참일시, 각 행을 식별하는 이름이 붙음)
     """
-    result_list = [['날짜'], ['식단'],
-                   ['에너지(kcal)'], ['탄수화물(g)'], ['단백질(g)'], ['지방(g)'], ['비타민A(R.E)'], ['티아민(mg)'],
-                   ['리보플라빈(mg)'], ['비타민C(mg)'], ['칼슘(mg)'], ['철분(mg)']]
+    result_list = [[] for i in range(12)]
     sheet_len, i = len(sheet), 0
+
+    if add_row_header:
+        headers = ['날짜', '식단', '에너지(kcal)', '탄수화물(g)', '단백질(g)', '지방(g)', '비타민A(R.E)', '티아민(mg)',
+         '리보플라빈(mg)', '비타민C(mg)', '칼슘(mg)', '철분(mg)']
+        for i in range(len(headers)):
+            result_list[i].append(headers[i])
 
     while True:
         while sheet[i][0] != '주간\n학교급식 영양량':
@@ -47,5 +52,5 @@ if __name__ == '__main__':
     # print(scan_xls_files(r"C:\Users\user\Documents\Math_Statistics_Project\학교 급식"))
     li = xls_file_to_list(r"C:\Users\user\Documents\Math_Statistics_Project\학교 급식\12_1월 학교급식 원산지 및 영양표시제.xls")
     # print(*li, sep="\n")
-    re_li = list_data_preprocessing(li)
+    re_li = list_data_preprocessing(li, add_row_header=False)
     print(*re_li, sep="\n")
