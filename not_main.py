08de7556f18d72f38d0a460b865c3f5be7dcb44b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-29 15:01:37 +0900

    code : add not_main.py

--- not_main.py
+++ not_main.py
@@ -1,5 +1,8 @@
-import pandas as pd
+# from openpyxl import load_workbook    (load_workbook 으로 xlsx 불러오기)
+import xlrd
 
-# xlrd 라이브러리 다운받기!
-path = r"C:\Users\user\Documents\Math_Statistics_Project\학교 급식"
-data = pd.read_excel(path + r"\12_1월 학교급식 원산지 및 영양표시제.xls", sheet_name=0)
+load_wb = xlrd.open_workbook("학교 급식/12_1월 학교급식 원산지 및 영양표시제.xls")
+load_ws = load_wb.sheet_by_index(0)
+
+for i in range(load_ws.nrows):
+    print(load_ws.row_values(i))
